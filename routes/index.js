fe04274f39e6b229918e5fd4f92076463db755bf
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-11-09 14:28:58 +0100

    add animation and design main.css

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Filo" });
+  res.render("index", { title: "Filo" , user: null, login: true});
 });
 module.exports = router;
